38aa606f70c06d6547685b72bbb6ab2f1750f2d3
akildevs1002 <akildevs1002@email.com>
2023-02-25 19:00:57 +0400

    fixed

--- backend/app/Models/RoomType.php
+++ backend/app/Models/RoomType.php
@@ -2,20 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class RoomType extends Model
 {
     use HasFactory;
 
-    /**
-     * Get the room associated with the RoomType
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasOne
-     */
+    protected $fillable = [
+        'weekday_price',
+        'weekend_price',
+        'holiday_price',
+        'company_id',
+    ];
+
     public function room()
     {
         return $this->hasOne(Room::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Order by name ASC
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder->orderBy('id', 'desc');
+        });
     }
 }
