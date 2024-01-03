d3299d94a472394d3caa0c46cffe32be8c7fd297
akildevs1002 <akildevs1002@email.com>
2024-01-03 16:33:40 +0400

    wordpress bulk booking

--- backend/routes/api_widget.php
+++ backend/routes/api_widget.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\BookingController;
 use App\Http\Controllers\WidgetApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('widget_rooms_availability', WidgetApiController::class);
+//Route::apiResource('widget_rooms_availability', WidgetApiController::class);
 Route::post('widget_rooms_availability', [WidgetApiController::class, 'getAvailableRoomList']);
 Route::get('widget_rooms_availability', [WidgetApiController::class, 'getAvailableRoomList']);
 Route::post('widget_api_room_booking', [BookingController::class, 'widgetRoomBooking']);
