ffdc3ddfde9983e7f18f51f30b3992e46db5a2c1
akildevs1002 <akildevs1002@email.com>
2024-01-13 17:49:11 +0400

    room light

--- backend/routes/devices.php
+++ backend/routes/devices.php
@@ -7,5 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/devices', DeviceController::class);
 Route::get('devices', [DeviceController::class, 'index']);
 Route::post('update_device_room_fill_status', [DeviceController::class, 'updateDevicRoomFileStatus']);
+Route::get('update_device_room_fill_status', [DeviceController::class, 'updateDevicRoomFileStatus']);
+
 Route::get('devices_logs', [DeviceController::class, 'getDevicesLogs']);
 Route::get('devices_list', [DeviceController::class, 'getDevicesList']);
