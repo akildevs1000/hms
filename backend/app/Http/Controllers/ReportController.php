614fad25c2acaa9c7c939b0c6e008bca18bcd7f5
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-07-05 19:29:12 +0400

    test

--- backend/app/Http/Controllers/ReportController.php
+++ backend/app/Http/Controllers/ReportController.php
@@ -691,29 +691,29 @@ class ReportController extends Controller
 
         $totalSum = $data->sum('revenue'); // Total sum of total_price across all sources
 
-        // Calculate percentage for each source
-        foreach ($data as $item) {
-            $percentage = ($item->revenue / $totalSum) * 100;
-            $item->percentage = round($percentage, 2) . "%"; // Rounded to 2 decimal places
-        }
-
         $colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf", "#ffbb78", "#aec7e8", "#ff9896"];
 
-        foreach ($data as $index => &$item) {
-            // Calculate percentage
-            $percentage = ($item['revenue'] / $totalSum) * 100;
-            $item['percentage'] = round($percentage, 2) . "%"; // Rounded to 2 decimal places
+        $newData = [];
 
-            // Apply colors
-            $colorIndex = $index % count($colors);
-            $item['background'] = $colors[$colorIndex];
-            $item['color'] = $colors[$colorIndex];
+        foreach ($data as $index => $item) {
+
+            $percentage = ($item['revenue'] / $totalSum) * 100;
+            $item['percentage'] =  // Rounded to 2 decimal places
+                $colorIndex = $index % count($colors);
+
+            $newData[] = [
+                "source" => $item["source"],
+                "no_of_room" => $item["no_of_room"],
+                "revenue" => $item["revenue"],
+                "percentage" => round($percentage, 2) . "%",
+                "color" => $colors[$colorIndex],
+            ];
         }
 
 
         return [
             "colors" => $colors,
-            "data" => $data,
+            "data" => $newData,
         ];
 
         // $response = Http::withoutVerifying()->get('https://backend.ezhms.com/api/get_source_rate_by_month', [
