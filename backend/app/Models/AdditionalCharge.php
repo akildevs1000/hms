5b1e1c1ade12746e2edf06f6b8b922b74c8f4e0d
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-08-05 19:11:02 +0400

    added

--- backend/app/Models/AdditionalCharge.php
+++ backend/app/Models/AdditionalCharge.php
@@ -10,4 +10,41 @@ class AdditionalCharge extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    /**
+     * Get the early check-in attribute.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function getEarlyCheckInAttribute($value)
+    {
+        // Customize the retrieval logic for early_check_in attribute
+        // For example, you might want to format the value or perform some other logic
+        return $value ?? 0;
+    }
+
+    /**
+     * Get the late check-out attribute.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function getLateCheckOutAttribute($value)
+    {
+        // Customize the retrieval logic for late_check_out attribute
+        return $value ?? 0;
+    }
+
+    /**
+     * Get the extra bed attribute.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function getExtraBedAttribute($value)
+    {
+        // Customize the retrieval logic for extra_bed attribute
+        return $value ?? 0;
+    }
 }
