d2cc9dda337dda767d0bc3f7536c91ca1b8cef6c
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-08-19 20:26:15 +0400

    Update RoomController.php

--- backend/app/Http/Controllers/RoomController.php
+++ backend/app/Http/Controllers/RoomController.php
@@ -347,7 +347,7 @@ class RoomController extends Controller
         $expectCheckOut = Room::with('device')
             ->whereHas('roomType', fn($q) => $q->where('type', request("type", "room")))
             ->whereHas('bookedRoom', function ($query) use ($company_id, $todayDate) {
-                $query->whereDate('check_in', '<=', $todayDate);
+                // $query->whereDate('check_in', '<=', $todayDate);
                 $query->whereDate('check_out', date("Y-m-d"));
 
                 $query->where('company_id', $company_id);
