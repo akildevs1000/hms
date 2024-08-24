2d357b931e890ecd91020b9ce7081120ee1ce067
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-08-24 14:24:11 +0400

    test

--- backend/app/Models/Verification.php
+++ backend/app/Models/Verification.php
@@ -10,4 +10,28 @@ class Verification extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function getIdFrontendSideAttribute($value)
+    {
+        if (!$value) return null;
+        return asset('id_frontend_side/' . $value);
+    }
+
+    public function getIdBackendSideAttribute($value)
+    {
+        if (!$value) return null;
+        return asset('id_backend_side/' . $value);
+    }
+
+    public function getCapturedPhotoAttribute($value)
+    {
+        if (!$value) return null;
+        return asset('captured_photo/' . $value);
+    }
+
+    public function getSignAttribute($value)
+    {
+        if (!$value) return null;
+        return asset('sign/' . $value);
+    }
 }
