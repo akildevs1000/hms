ff51d9fdf32ecceacf03b34077371b31d5c824e0
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-09-18 16:01:19 +0400

    Update RoomController.php

--- backend/app/Http/Controllers/RoomController.php
+++ backend/app/Http/Controllers/RoomController.php
@@ -407,7 +407,7 @@ class RoomController extends Controller
                 })
                     // Option 2: Exclude rooms based on booking status
                     ->orWhere(function ($query) {
-                        $query->where('booking_status', ">=", 2);
+                        $query->whereIn('booking_status', [2, 3]);
                     });
             })
             ->get();
