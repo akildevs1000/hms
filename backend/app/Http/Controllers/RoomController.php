cf3c1419b44b6dbb42f3c232bd29ea4e7db89324
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-09-18 15:43:19 +0400

    test

--- backend/app/Http/Controllers/RoomController.php
+++ backend/app/Http/Controllers/RoomController.php
@@ -398,9 +398,10 @@ class RoomController extends Controller
             ->where('status', '!=', Room::Blocked)
             ->whereDoesntHave('bookedRoom', function ($q) use ($company_id, $todayDate) {
                 $q->where('company_id', $company_id);
-                $q->whereDate("check_in", ">=", $todayDate);
-                $q->whereDate("check_out", "<=", date('Y-m-d', strtotime($todayDate . " +1 day")));
-                $q->where('booking_status', 0);
+                $q->whereDate('check_out', $todayDate);
+                // $q->whereDate("check_in", ">=", $todayDate);
+                // $q->whereDate("check_out", "<=", date('Y-m-d', strtotime($todayDate . " +1 day")));
+                // $q->where('booking_status', '!=', 0);
             })
             ->get();
 
