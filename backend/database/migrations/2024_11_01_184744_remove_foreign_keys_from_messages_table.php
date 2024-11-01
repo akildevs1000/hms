62fac388ae0772ba6cf6e3c723a4a5a4a4ce7f74
akildevs1000 <akildevs1000@gmail.com>
2024-11-01 18:48:46 +0400

    remove foriegn id from users table

---
+++ backend/database/migrations/2024_11_01_184744_remove_foreign_keys_from_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            // Drop the foreign key constraints
+            $table->dropForeign(['sender_id']);
+            $table->dropForeign(['receiver_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            // Optionally, add back the foreign keys if you need to roll back
+            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+};
