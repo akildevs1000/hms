326437aeed76903f1c48ecfe32e28b4beadf4cb9
akildevs1000 <akildevs1000@gmail.com>
2024-11-29 12:18:14 +0400

    test

--- backend/app/Http/Controllers/AdminExpenseController.php
+++ backend/app/Http/Controllers/AdminExpenseController.php
@@ -37,6 +37,7 @@ class AdminExpenseController extends Controller
             ]
         )
             ->where("is_admin_expense", request("is_admin_expense", AdminExpense::NonManagementExpense))
+            ->where("company_id", request("company_id"))
             ->paginate(request("per_page", 50));
     }
 
