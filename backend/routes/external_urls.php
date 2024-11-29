c76102a215c720196f23fed1f9681539e971a1ec
akildevs1000 <akildevs1000@gmail.com>
2024-11-29 21:17:29 +0400

    test

--- backend/routes/external_urls.php
+++ backend/routes/external_urls.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('whatsapp-call', [ExternalUrlController::class, 'whatsappCall']);
 
+
+Route::post('/send-message', [ExternalUrlController::class, 'sendMessage']);
