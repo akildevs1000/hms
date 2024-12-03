8c24e29d45770b58b5cdc22028015f0ef396f7ee
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-12-04 01:13:31 +0400

    tet

--- backend/app/Http/Controllers/RoomController.php
+++ backend/app/Http/Controllers/RoomController.php
@@ -485,7 +485,7 @@ class RoomController extends Controller
                 $query->whereDate('check_out', $todayDate)
                     ->orWhereDate('check_in', $todayDate);
             })
-            ->whereIn('booking_status', [1, 2])
+            ->whereIn('booking_status', [BookedRoom::CHECKED_IN])
             ->selectRaw("
             SUM(CASE WHEN DATE(check_in) = ? THEN breakfast ELSE 0 END) as occupied_breakfast,
             SUM(CASE WHEN DATE(check_in) = ? THEN lunch ELSE 0 END) as occupied_lunch,
