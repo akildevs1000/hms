9e5dc8709143d84f700dfa87099d9a0da8f7754a
akildevs1000 <akildevs1000@gmail.com>
2024-12-05 15:45:07 +0400

    test

--- backend/app/Http/Controllers/BookingController.php
+++ backend/app/Http/Controllers/BookingController.php
@@ -2769,16 +2769,8 @@ class BookingController extends Controller
 
         for ($i = 0; $i < 10; $i++) {
             $date = date("Y-m-d", strtotime("+$i days", strtotime($today)));
-            $AvailableRooms = Room::with("is_cleaned")
-            ->where('company_id', $id)
+            $AvailableRooms = Room::where('company_id', $id)
             ->whereNot("status", Room::Blocked)
-            ->whereDoesntHave("bookedRoom", function ($query) use ($today, $id) {
-                $query->where(function ($query) use ($today) {
-                    $query->whereDate('check_in', ">=",  $today)
-                        ->orWhereDate('check_in', "<=",  $today);
-                })
-                    ->where('company_id', $id);
-            })
             ->count();
             $dates[$date] = [
                 "label" => date("D", strtotime($date)),
