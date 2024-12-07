3022268f70fdb1c90f6b922c8f75444224d0f649
akildevs1000 <akildevs1000@gmail.com>
2024-12-07 13:21:24 +0400

    test

--- frontend/pages/expense.vue
+++ frontend/pages/expense.vue
@@ -1,11 +1,10 @@
 <template>
-  <div v-if="can(`expense_access`)">
+  <div>
     <v-tabs right>
-      <v-tab v-if="can(`management_expense_access`)">Management Expense </v-tab>
-      <v-tab>Non Management Expense</v-tab>
-      <v-tab>Vendor</v-tab>
-      <v-tab>Category</v-tab>
-
+      <v-tab v-if="can(`management_expense_access`)">Management</v-tab>
+      <v-tab v-if="can(`expense_access`)">Expense</v-tab>
+      <v-tab v-if="can(`vendors_access`)">Vendor</v-tab>
+      <v-tab v-if="can(`expense_category_access`)">Category</v-tab>
       <v-tab-item>
         <Expense :is_admin_expense="1" />
       </v-tab-item>
@@ -14,10 +13,8 @@
       </v-tab-item>
       <v-tab-item><Vendor /></v-tab-item>
       <v-tab-item><VendorCategory /></v-tab-item>
-
     </v-tabs>
   </div>
-  <NoAccess v-else />
 </template>
 
 <script>
