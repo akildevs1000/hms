73752840e48b1c2d6d8d34616936b5db9b3dc082
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-12-07 17:10:30 +0400

    food_plan_price

--- backend/app/Http/Controllers/InvoiceRecalWithoutFoodController.php
+++ backend/app/Http/Controllers/InvoiceRecalWithoutFoodController.php
@@ -86,8 +86,8 @@ class InvoiceRecalWithoutFoodController   extends Controller
             $cgst = '';
             $total_with_tax =   $room->grand_total; //+ $room->early_check_in; //3000
 
-            // if (count($orderRooms) > 1)
-            //     $total_with_tax =   $room->grand_total + $room->early_check_in + ($room->food_plan_price / count($orderRooms)); //3000
+            if (count($orderRooms) > 1)
+                $total_with_tax =   $room->grand_total + $room->early_check_in - ($room->food_plan_price / count($orderRooms)); //3000
             // return [$total_with_tax, $room->grand_total, $room->early_check_in, ($room->food_plan_price / count($orderRooms))];
             $BookingObj = new BookingController();
             $room_tax =   $BookingObj->getTaxSlab(($total_with_tax + 900), $room->company_id);
