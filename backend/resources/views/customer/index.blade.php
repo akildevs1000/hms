19927c8dffb647397dec4311b53c1ad9143f0522
akildevs1000 <akildevs1000@gmail.com>
2025-03-28 18:38:14 +0400

    test

--- backend/resources/views/customer/index.blade.php
+++ backend/resources/views/customer/index.blade.php
@@ -265,6 +265,17 @@
 
     <hr>
 
+
+    <div class="row mt-3">
+        <div class="col-12">
+            <label for="name" class="label-txt">Customer Sign:</label>
+            <br>
+            <img style="height: 100px" src="{{ $booking->customer->sign ?? '' }}" alt="">
+        </div>
+    </div>
+
+    <hr>
+
     <div class="row mt-3">
         <div class="col-12">
             <label for="name" class="label-txt">Customer ID (Front):</label>
