db533ea3f19bce5f8caf798e5d84bda9eb358f97
akildevs1000 <akildevs1000@gmail.com>
2025-04-03 19:46:06 +0400

    test

--- backend/app/Console/Commands/CheckMail.php
+++ backend/app/Console/Commands/CheckMail.php
@@ -27,7 +27,7 @@ class CheckMail extends Command
 
         try {
             // Attempt to send the email
-            Mail::to($testEmail)->queue(new PdfMail());
+            Mail::to($testEmail)->send(new PdfMail());
 
             $this->info('Test email sent successfully!');
         } catch (\Exception $e) {
