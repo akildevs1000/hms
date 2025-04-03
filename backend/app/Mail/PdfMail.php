989bd8d4c75b395da72f606e3d5673ce7006be0c
akildevs1000 <akildevs1000@gmail.com>
2025-04-03 19:49:04 +0400

    Update PdfMail.php

--- backend/app/Mail/PdfMail.php
+++ backend/app/Mail/PdfMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class PdfMail extends Mailable implements ShouldQueue
+class PdfMail extends Mailable
 {
     use Queueable, SerializesModels;
 
