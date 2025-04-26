ac6bb3ea71a329e755fa234d604e756998ad6981
akildevs1000 <akildevs1000@gmail.com>
2025-04-26 15:54:03 +0400

    added command when customer arrived

---
+++ backend/app/Console/Commands/Automation/WhenCustomerArrived.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands\Automation;
+
+use App\Http\Controllers\Controller;
+use App\Jobs\EmailSender;
+use App\Jobs\WhatsappSender;
+use App\Models\Template;
+use Illuminate\Console\Command;
+
+class WhenCustomerArrived extends Command
+{
+    protected $signature = 'when_customer_arrived';
+
+    protected $description = 'when_customer_arrived';
+
+    protected $templates = [];
+
+    protected $tags = [];
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $payload = [
+            "command" => Template::WHEN_CUSTOMER_ARRIVED,
+            "heading" => "WHEN_CUSTOMER_ARRIVED",
+            "company_id" => 1,
+            "whatsapp" => "971554501483",
+            "email" => "francisgill1000@gmail.com",
+
+            "fields" => [
+                'title' => "Mr",
+                'full_name' => "francis",
+                'from_date' => date("d-M-y"),
+                'to_date' => date("d-M-y"),
+            ]
+        ];
+
+        if ($payload["whatsapp"]) {
+            WhatsappSender::dispatch([
+                'recipient' => $payload["whatsapp"],
+                'text' => (new Controller)->prepareMessage($payload['fields'], "whatsapp", $payload["command"]),
+                'company_id' => $payload["company_id"],
+            ]);
+        }
+
+        if ($payload["email"]) {
+            EmailSender::dispatch([
+                'recipient' => $payload["email"],
+                'text' => (new Controller)->prepareMessage($payload['fields'], "email", $payload["command"]),
+                'company_id' => $payload["company_id"],
+                'heading' => $payload["heading"],
+            ]);
+        }
+
+        echo json_encode($payload, JSON_PRETTY_PRINT);
+    }
+}
