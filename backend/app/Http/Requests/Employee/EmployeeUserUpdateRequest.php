042bd12ffba0e06a98e8e855cda022a74adfa833
akildevs1000 <akildevs1000@gmail.com>
2025-07-24 15:29:31 +0400

    test

--- backend/app/Http/Requests/Employee/EmployeeUserUpdateRequest.php
+++ backend/app/Http/Requests/Employee/EmployeeUserUpdateRequest.php
@@ -34,10 +34,10 @@ class CompanyUserUpdateRequest extends FormRequest
                 'confirmed',
                 'min:6',             // must be at least 10 characters in length
                 'max:25',             // must be maximum 25 characters in length
-                'regex:/[a-z]/',      // must contain at least one lowercase letter
-                'regex:/[A-Z]/',      // must contain at least one uppercase letter
-                'regex:/[0-9]/',      // must contain at least one digit
-                'regex:/[@$!%*#?&]/', // must contain a special character
+                // 'regex:/[a-z]/',      // must contain at least one lowercase letter
+                // 'regex:/[A-Z]/',      // must contain at least one uppercase letter
+                // 'regex:/[0-9]/',      // must contain at least one digit
+                // 'regex:/[@$!%*#?&]/', // must contain a special character
             ],
         ];
     }
