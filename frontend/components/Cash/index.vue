6b332d61681dbe39796b58cdf65a194e3142aa16
akildevs1000 <akildevs1000@gmail.com>
2025-07-30 16:14:48 +0400

    fixed cash reprot

---
+++ frontend/components/Cash/index.vue
@@ -0,0 +1,269 @@
+<template>
+  <v-card class="mb-5 rounded-md mt-0" elevation="0">
+    <!-- <v-toolbar
+                    class="rounded-md"
+                    color="background"
+                    dense
+                    flat
+                    dark
+                  >
+                    <v-spacer></v-spacer>
+                    <v-tooltip top color="primary">
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                          class="ma-0"
+                          x-small
+                          :ripple="false"
+                          text
+                          v-bind="attrs"
+                          v-on="on"
+                          @click="process('income_report_print')"
+                        >
+                          <v-icon class="">mdi-printer-outline</v-icon>
+                        </v-btn>
+                      </template>
+                      <span>PRINT</span>
+                    </v-tooltip>
+
+                    <v-tooltip top color="primary">
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                          x-small
+                          :ripple="false"
+                          text
+                          v-bind="attrs"
+                          v-on="on"
+                          @click="process('income_report_download')"
+                        >
+                          <v-icon class="">mdi-download-outline</v-icon>
+                        </v-btn>
+                      </template>
+                      <span> DOWNLOAD </span>
+                    </v-tooltip>
+                  </v-toolbar> -->
+    <v-container fluid>
+      <v-row dense>
+        <v-col cols="12">
+          <table cellspacing="0" style="width: 100%">
+            <AssetsTableHeader :cols="incomeHeaders" />
+            <tbody>
+              <tr v-for="(item, index) in incomeData" :key="index">
+                <td class="text-center py-2 border-bottom">
+                  {{ index + 1 }}
+                </td>
+                <td class="text-center py-2 border-bottom">
+                  {{ item.booking_date }}
+                </td>
+                <td class="text-center py-2 border-bottom">
+                  <span @click="goToRevView(item)" style="cursor: pointer">
+                    {{ item?.reservation_no || "---" }}
+                  </span>
+                </td>
+                <td class="text-center py-2 border-bottom">
+                  {{
+                    Array.isArray(item?.order_rooms) && item?.order_rooms.length
+                      ? item?.order_rooms.map((e) => e.room_no).join(", ")
+                      : "---"
+                  }}
+                </td>
+
+                <td class="text-center py-2 border-bottom">
+                  {{ item && item?.customer && item.customer?.first_name }}
+                </td>
+                <td class="text-right py-2 border-bottom">
+                  {{ $utils.currency_format(item?.cash_sum_amount || 0) }}
+                </td>
+              </tr>
+            </tbody>
+
+            <tr v-if="allTotalProcessed">
+              <td colspan="5" class="py-2 border-bottom">Total</td>
+              <td class="text-right py-2 border-bottom">
+                {{ $utils.currency_format(totals.cash) }}
+              </td>
+            </tr>
+          </table>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["filters"],
+  data: () => ({
+    Model: "Expense",
+    vertical: false,
+    activeTab: 0,
+    from_date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+      .toISOString()
+      .substr(0, 10),
+    from_menu: false,
+    to_date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+      .toISOString()
+      .substr(0, 10),
+    to_menu: false,
+    pagination: {
+      current: 1,
+      total: 0,
+      per_page: 10,
+      status: "-1",
+    },
+    options: {},
+    endpoint: "payments",
+    search: "",
+    snackbar: false,
+    dialog: false,
+    expenseData: [],
+    incomeData: [],
+    counts: [],
+    loading: false,
+    incomeHeaders: [
+      { align: "center", text: "#" },
+      { align: "center", text: "Date" },
+      { align: "center", text: "Rev. No" },
+      { align: "center", text: "Rooms" },
+      { align: "center", text: "Guest" },
+      { align: "right", text: "Cash" },
+    ],
+    editedIndex: -1,
+    response: "",
+    loss: "",
+    profit: "",
+    errors: [],
+    search: null,
+    editedItem: {
+      item: null,
+      amount: null,
+      payment_modes: "CASH",
+    },
+    incomeStats: {
+      Cash: 0,
+      Card: 0,
+      Online: 0,
+      Bank: 0,
+      UPI: 0,
+      Cheque: 0,
+      CityLedger: 0,
+    },
+    totals: {
+      cash: 0,
+      card: 0,
+      online: 0,
+      bank: 0,
+      upi: 0,
+      cheque: 0,
+      pending: 0,
+    },
+    allTotalProcessed: false,
+  }),
+  created() {
+    this.getData();
+  },
+  watch: {
+    filters: {
+      handler(data) {
+        this.from_date = data.from;
+        this.to_date = data.to;
+        this.search = data.search;
+        if (this.from_date && this.to_date) {
+          this.getData();
+        }
+      },
+      deep: true,
+      immediate: true, // Optional: triggers the watcher immediately on component mount
+    },
+  },
+  computed: {},
+  methods: {
+    getTotalCash(data) {
+      this.allTotalProcessed = false;
+
+      const totals = {
+        cash: 0,
+        card: 0,
+        online: 0,
+        bank: 0,
+        upi: 0,
+        cheque: 0,
+        pending: 0,
+      };
+
+      for (const item of data) {
+        totals.cash += Number(item?.cash_sum_amount) || 0;
+        totals.card += Number(item?.card_sum_amount) || 0;
+        totals.online += Number(item?.online_sum_amount) || 0;
+        totals.bank += Number(item?.bank_sum_amount) || 0;
+        totals.upi += Number(item?.upi_sum_amount) || 0;
+        totals.cheque += Number(item?.cheque_sum_amount) || 0;
+        totals.pending += Number(item?.pending_sum_amount) || 0;
+      }
+
+      this.totals = totals;
+      this.allTotalProcessed = true;
+    },
+
+    caps(str) {
+      if (str == "" || str == null) {
+        return "---";
+      } else {
+        let res = str.toString();
+        return res.replace(/\b\w/g, (c) => c.toUpperCase());
+      }
+    },
+    goToRevView(item) {
+      // this.$router.push(`/customer/details/${item.booking.id}`);
+    },
+    process(type) {
+      let comId = this.$auth.user.company.id; //company id
+      let from = this.from_date;
+      let to = this.to_date;
+      let url = `https://backend.myhotel2cloud.com/api/${type}?company_id=${comId}&from=${from}&to=${to}`;
+      console.log(url);
+      let element = document.createElement("a");
+      element.setAttribute("target", "_blank");
+      element.setAttribute("href", `${url}`);
+      document.body.appendChild(element);
+      element.click();
+    },
+    incomeByPaymentMode(item, paymentModeKey) {
+      let payments = item.payments;
+      payments.forEach((e) => {
+        const paymentMode = (e?.payment_mode?.name ?? "Cash").replace(" ", "");
+        if (paymentMode === paymentModeKey) {
+          console.log("🚀 ~ payments.forEach ~ paymentMode:", paymentMode);
+          console.log("🚀 ~ payments.forEach ~ e:", parseFloat(e?.amount || 0));
+          return parseFloat(e?.amount || 0);
+        } else {
+          return 0;
+        }
+      });
+    },
+    getData() {
+      if (this.loading) return false;
+      this.loading = true;
+      let options = {
+        params: {
+          page: this.pagination.current,
+          status: this.pagination.status,
+          per_page: this.pagination.per_page,
+          company_id: this.$auth.user.company.id,
+          from_date: this.from_date,
+          to_date: this.to_date,
+          search: this.search,
+        },
+      };
+
+      this.$axios.get(this.endpoint, options).then(({ data }) => {
+        this.incomeData = data.data;
+
+        this.getTotalCash(data.data);
+        this.incomeStats = data.stats;
+        this.$emit("stats", data.stats);
+        this.loading = false;
+      });
+    },
+  },
+};
+</script>
--- frontend/pages/management/report/cash.vue
+++ frontend/pages/management/report/cash.vue
@@ -1,79 +1,89 @@
 <template>
-  <div v-if="can(`cash_access`)">
-    <v-card class="pa-5">
-      <v-row>
-        <v-col cols="10"></v-col>
-        <v-col cols="2">
-          <FilterDateRange @filter-attr="filterAttr" />
-        </v-col>
-        <v-col md="12" class="text-right">
-          <AssetsTable height="500" :headers="headers" :items="items">
-            <template #row>
-              <tr v-if="summaryRow">
-                <td class="text-center black--text py-1">Total</td>
-                <td
-                  class="text-center black--text py-1"
-                  v-for="(row, index) in summaryRow"
-                  :key="index"
-                >
-                  {{ row }}
-                </td>
-              </tr>
-            </template>
-          </AssetsTable>
-        </v-col>
-      </v-row>
-    </v-card>
+  <div v-if="can('income_access')">
+    <v-row dense>
+      
+      <v-col cols="12">
+        <v-card class="my-2">
+          <div
+            style="display: flex; justify-content: right; align-items: right"
+            class="pa-2"
+          >
+            <div style="padding-top: 1px">
+              <v-text-field
+                label="Search..."
+                dense
+                outlined
+                flat
+                append-icon="mdi-magnify"
+                v-model="search"
+                @input="searchIt"
+                hide-details
+                style="max-width: 200px"
+              ></v-text-field>
+            </div>
+            &nbsp;
+            <FilterDateRange :defaultDates="true" @filter-attr="filterAttr" />
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <Cash
+          :filters="filters"
+          @stats="handleIncome"
+        />
+      </v-col>
+    </v-row>
   </div>
   <NoAccess v-else />
 </template>
 
 <script>
-let date = new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-  .toISOString()
-  .substr(0, 10);
-
 export default {
   data: () => ({
-    stats: [],
-    from_date: date,
-    to_date: date,
-    from_menu: false,
-    options: {},
-    search: "",
-    snackbar: false,
-    dialog: false,
-    loading: true,
-    payloadOptions: {
-      from_date: date,
-      to_date: date,
+    search: null,
+    filters: {
+      from: new Date().toJSON().slice(0, 10),
+      to: new Date().toJSON().slice(0, 10),
     },
-    items: [],
-    headers: [],
-    summaryRow: null,
-  }),
+    IncomeCardDialog: false,
+    ExpenseCardDialog: false,
+    ManagementCardDialog: false,
+    ProfitLossCardDialog: false,
 
-  created() {
-    this.getdata();
+    currentTabId: 1,
+    key: 1,
+    colors: [
+      "#92d050",
+      "#ff0000",
+      "#ffc000",
+      "#0D652D",
+      "#174EA6",
+      "#2E3945",
+      "#2ECC71",
+      "#CE0E2D",
+      "#0077B5",
+    ],
+    activeTab: 0,
+    income: null,
+    expense: null,
+    managementExpense: null,
+    loading: false,
+    ProfilLoss: null,
+  }),
+  async created() {
+    this.loading = true;
+    await this.getProfitLoss();
   },
 
   methods: {
-    filterAttr(data) {
-      this.payloadOptions = {
-        from_date: data.from,
-        to_date: data.to,
-      };
-
-      this.getdata();
+    handleIncome(e) {
+      this.income = e;
     },
-    openExternalLink(path) {
-      let url = `https://backend.myhotel2cloud.com/api/get_audit_report_print?path=${path}`;
-      let element = document.createElement("a");
-      element.setAttribute("target", "_blank");
-      element.setAttribute("href", url);
-      document.body.appendChild(element);
-      console.log(element);
-      element.click();
+    handleNonMagementExpense(e) {
+      this.expense = e;
+    },
+    handleMagementExpense(e) {
+      this.managementExpense = e;
     },
     can(per) {
       let u = this.$auth.user;
@@ -81,53 +91,40 @@ export default {
         (u && u.permissions.some((e) => e == per || per == "/")) || u.is_master
       );
     },
+    filterAttr(data) {
+      this.filters = {
+        from: data.from,
+        to: data.to,
+        search: this.search,
+      };
 
-    getSum(item, type) {
-      let sum = 0;
-      item.map((e) => {
-        e.transactions.map((e) =>
-          e.payment_method_id == type ? (sum += parseFloat(e.credit)) : 0
-        );
-      });
-      return sum.toFixed(2);
+      this.getProfitLoss();
     },
-
-    async getdata() {
-      this.loading = true;
-      const url = "cash-report";
-      this.payloadOptions.company_id = this.$auth.user.company_id;
-
-      try {
-        const response = await this.$axios.get(url, {
-          params: { ...this.payloadOptions },
-        });
-
-        const { data } = response;
-        // Update headers and items
-        this.headers = data.headers || [];
-        this.items =
-          data?.data?.map((e) => {
-            return {
-              id: e.id,
-              date: e.date,
-              sold: e.data.displayValues.sold,
-              cash: e.data.displayValues.cash,
-              expense: e.data.displayValues.expense,
-              balance: e.data.displayValues.balance,
-            };
-          }) || [];
-
-        this.summaryRow = data.summaryRow;
-
-        this.summaryRow.balance = this.$utils.currency_format(
-          data?.data?.reduce((acc, cur) => acc + cur.data.balance, 0)
-        );
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        // Optionally show an error message to the user
-      } finally {
-        this.loading = false; // Ensure loading state is updated
+    searchIt() {
+      if (this.search.length == 0) {
+        this.filters = {
+          ...this.filters,
+          search: this.search,
+        };
+      } else if (this.search.length > 2) {
+        this.filters = {
+          ...this.filters,
+          search: this.search,
+        };
       }
+    },
+    async getProfitLoss() {
+      let config = {
+        params: {
+          company_id: this.$auth.user.company_id,
+          from_date: this.filters.from,
+          to_date: this.filters.to,
+        },
+      };
+
+      let { data } = await this.$axios.get(`profit-loss`, config);
+
+      this.ProfilLoss = data;
     },
   },
 };
