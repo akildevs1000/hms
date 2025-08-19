f4c918af638b9319b1464bc00655581cf305b325
akildevs1000 <akildevs1000@gmail.com>
2025-08-19 19:15:12 +0400

    added floor crud on frontend

---
+++ frontend/components/Floor/Delete.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <v-icon color="black" small @click="submit"> mdi-delete </v-icon>
+      Delete
+    </div>
+  </template>
+  <script>
+  export default {
+    props: ["id", "endpoint"],
+    methods: {
+      async submit() {
+        try {
+          await this.$axios.delete(this.endpoint + "/" + this.id);
+          this.$emit("response", "Record has been delete");
+        } catch ({ response }) {
+          console.log(response);
+          // console.error("Error adding income:", response.data.message);
+        }
+      },
+    },
+  };
+  </script>
---
+++ frontend/components/Floor/index.vue
@@ -0,0 +1,144 @@
+<template>
+  <v-data-table
+    dense
+    :headers="headers"
+    :items="floors"
+    :loading="loading"
+    :options.sync="options"
+    :footer-props="{
+      itemsPerPageOptions: [100, 500, 1000],
+    }"
+  >
+    <template v-slot:top>
+      <v-toolbar flat dense class="mb-5">
+        {{ Model }}
+        <v-icon color="primary" right @click="getDataFromApi()"
+          >mdi-reload</v-icon
+        >
+        <v-spacer></v-spacer>
+        <FloorCreate
+          :model="Model"
+          :endpoint="endpoint"
+          @response="getDataFromApi"
+        />
+      </v-toolbar>
+    </template>
+    <template v-slot:item.is_for_hall="{ item }">
+      {{ item.is_for_hall ? "Hall" : "Room" }}
+    </template>
+
+    <template v-slot:item.options="{ item }">
+      <v-menu bottom left>
+        <template v-slot:activator="{ on, attrs }">
+          <div class="text-center">
+            <v-btn dark-2 icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </div>
+        </template>
+        <v-list width="120" dense>
+          <v-list-item>
+            <v-list-item-title>
+              <FloorView
+                :model="Model"
+                :endpoint="endpoint"
+                :item="item"
+                @response="getDataFromApi"
+              />
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>
+              <FloorEdit
+                :model="Model"
+                :endpoint="endpoint"
+                :item="item"
+                @response="getDataFromApi"
+              />
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>
+              <FloorDelete
+                :id="item.id"
+                :endpoint="endpoint"
+                @response="getDataFromApi"
+              />
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+let date = new Date();
+
+let d = date.getDate();
+let m = (date.getMonth() + 1).toString().padStart(2, "0");
+let y = date.getFullYear();
+let currentDate = y + "-" + m + "-" + d;
+
+export default {
+  data: () => ({
+    Model: "Floors",
+    endpoint: "floors",
+    currentDate,
+    filters: {},
+    options: {},
+    loading: false,
+    response: "",
+    floors: [],
+    errors: [],
+    headers: [
+      {
+        text: "Floor Number",
+        value: "number",
+      },
+      {
+        text: "Floor Name",
+        value: "name",
+      },
+      {
+        text: "Action",
+        align: "center",
+        sortable: false,
+        value: "options",
+      },
+    ],
+    componentKey: 1,
+  }),
+
+  async created() {
+    this.getDataFromApi();
+    // this.getVendorCategory();
+  },
+  mounted() {},
+  watch: {
+    options: {
+      handler() {
+        this.getDataFromApi();
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    getRandomId() {
+      return ++this.componentKey;
+    },
+    async getVendorCategory() {
+      let { data } = await this.$axios.get(this.endpoint);
+
+      return data;
+    },
+    async getDataFromApi() {
+      this.loading = true;
+      let { data } = await this.$axios.get(this.endpoint);
+      this.loading = false;
+
+      this.floors = data.data;
+    },
+  },
+};
+</script>
