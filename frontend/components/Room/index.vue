80d8edc9b1f0d99104c031c5dc23ae860ccb6bcd
Venu J <149071174+akildevs1004@users.noreply.github.com>
2025-08-25 16:23:27 +0400

    Testing

--- frontend/components/Room/index.vue
+++ frontend/components/Room/index.vue
@@ -480,6 +480,7 @@ export default {
           this.floors = data;
         });
     },
+
     updateQRCode() {
       this.data.forEach(async (e) => {
         let url = `${process.env.CUSTOMER_APP_URL}?company_id=${this.$auth.user.company.id}&room_id=${e.id}&room_no=${e.room_no}`;
