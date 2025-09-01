5349e13b258bb44b6115bf96ff59c75612ce83f1
akildevs1000 <akildevs1000@gmail.com>
2025-09-01 16:23:36 +0400

    added payment mdoe relation in transactions

--- backend/app/Http/Controllers/TransactionController.php
+++ backend/app/Http/Controllers/TransactionController.php
@@ -36,7 +36,7 @@ class TransactionController extends Controller
 
     public function getTransactionByBookingId(Request $request, $id)
     {
-        $transactions = Transaction::whereBookingId($id)->where('company_id', $request->company_id);
+        $transactions = Transaction::with("paymentMode")->whereBookingId($id)->where('company_id', $request->company_id);
 
         $totalTransactionAmount = $transactions->clone()->orderBy('id', 'desc')->first();
 
