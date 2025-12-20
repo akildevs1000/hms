44d92dc5f7f7a8dc989f2ac7ffc6a3546a1cdf52
akildevs1000 <akildevs1000@gmail.com>
2025-12-20 16:57:41 +0400

    fixed invoice

--- backend/resources/views/invoice/invoice_updated_with_tax.blade.php
+++ backend/resources/views/invoice/invoice_updated_with_tax.blade.php
@@ -330,7 +330,7 @@
                                                     class="tm_width_3 tm_primary_color tm_bold tm_text_right tm_border_none tm_pt0">
                                                     {{ $company->currency ? $company->currency : '' }}
 
-                                                    {{ number_format($subtotal_total, 2) }}
+                                                    {{ number_format(round($subtotal_total), 2, '.', ',') }}
                                                 </td>
                                             </tr>
                                             <tr>
@@ -351,7 +351,7 @@
                                                 <td
                                                     class="tm_width_3 tm_border_top_0 tm_bold   tm_primary_color tm_text_right tm_gray_bg tm_radius_0_6_6_0">
 
-                                                    {{ $company->currency ? $company->currency : '' }}{{ number_format($subtotal_total - $booking->paid_amounts, 2) }}
+                                                    {{ $company->currency ? $company->currency : '' }}{{ number_format(round($subtotal_total - $booking->paid_amounts), 2, '.', ',') }}
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -360,8 +360,8 @@
                             </div>
                             <div class="tm_text_right">
                                 <br>Amount:
-                                <?php 
-                                echo (new App\Http\Controllers\GRCController())->amountToText(round($subtotal_total, 2)); 
+                                <?php
+                                echo (new App\Http\Controllers\GRCController())->amountToText(round($subtotal_total, 2));
                                 ?>
                                 Only
                             </div>
